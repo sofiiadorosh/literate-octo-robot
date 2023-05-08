7652bcfe729e8be95ce22139eb7fbdc3434bafae
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-05-08 14:26:26 +0300

    init: set up project

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import { store } from './store';
+
+import App from './App';
+
+import './styles/base.scss';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
