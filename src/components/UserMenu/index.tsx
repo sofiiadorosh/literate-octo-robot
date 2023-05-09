0110a8fd5a0058c5afcfc33fe8965946973f84ec
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-05-09 19:27:48 +0300

    fix: add cart

--- src/components/UserMenu/index.tsx
+++ src/components/UserMenu/index.tsx
@@ -6,21 +6,20 @@ import { ReactComponent as Cart } from '@assets/cart.svg';
 
 import './UserMenu.scss';
 
-const navItems = [
-  { href: '/profile', icon: User },
-  { href: '/checkout', icon: Cart },
-];
-
 export const UserMenu: FC = () => {
   return (
     <ul className="user-list">
-      {navItems.map(({ href, icon: Icon }) => (
-        <li key={href} className="user-list__item">
-          <NavLink to={href} className="user-list__link">
-            <Icon className="user-list__icon" />
-          </NavLink>
-        </li>
-      ))}
+      <li className="user-list__item">
+        <NavLink to="/profile" className="user-list__link">
+          <User className="user-list__icon" />
+        </NavLink>
+      </li>
+      <li className="user-list__item">
+        <NavLink to="/checkout" className="user-list__link">
+          <Cart className="user-list__icon" />
+        </NavLink>
+        <span className="cart">4</span>
+      </li>
     </ul>
   );
 };
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,18 @@
 import React, { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { Container } from '@components/Container';
 
 const HomePage: FC = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Container>
+        <NavLink to="/products" style={{ fontSize: 20 }}>
+          Link to Products Page
+        </NavLink>
+      </Container>
+    </div>
+  );
 };
 
 export default HomePage;
