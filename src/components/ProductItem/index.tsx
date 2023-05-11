ed43f38687b4c72cd5ec2354649a471d7fdb3fe9
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-05-11 10:51:30 +0300

    refactor: add links

--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { Product } from '@types';
 
@@ -14,6 +15,7 @@ type ProductItemProps = {
 
 export const ProductItem: FC<ProductItemProps> = ({
   item: {
+    id,
     image,
     title,
     description,
@@ -29,31 +31,31 @@ export const ProductItem: FC<ProductItemProps> = ({
 }) => {
   return (
     <li className="product">
-      <div className="product__thumb">
+      <NavLink to={id} className="product__thumb">
         <img src={image} alt={title} className="product__image" />
-      </div>
+      </NavLink>
       <div className="product__content">
         <div className="product-info">
-          <h2 className="product-info__title">{title}</h2>
+          <h2 className="product-info__title">
+            <NavLink to={id} className="product__link">
+              {title}
+            </NavLink>
+          </h2>
           <p className="product-info__description">
             {description.substring(0, 51)}...
           </p>
           <ul className="product-info__rating">
-            <li className="rating-item">
-              <Star className="rating-item__icon" />
-            </li>
-            <li className="rating-item">
-              <Star className="rating-item__icon" />
-            </li>
-            <li className="rating-item">
-              <Star className="rating-item__icon" />
-            </li>
-            <li className="rating-item">
-              <Star className="rating-item__icon" />
-            </li>
-            <li className="rating-item">
-              <Star className="rating-item__icon" />
-            </li>
+            {[...Array(rating)].map((_, index) => (
+              <li key={index} className="rating-item">
+                <Star className="rating-item__icon--filled" />
+              </li>
+            ))}
+            {rating < 5 &&
+              [...Array(5 - rating)].map((_, index) => (
+                <li key={index} className="rating-item">
+                  <Star className="rating-item__icon--empty" />
+                </li>
+              ))}
           </ul>
           <ul className="product-info__list">
             <li className="product-info__item">
@@ -85,10 +87,10 @@ export const ProductItem: FC<ProductItemProps> = ({
             Delivery in {deliveryTime} days
           </span>
           <div className="product-order__button">
-            <button type="button" className="primary-button">
+            <NavLink to={id} className="primary-button">
               <span>Product Detail</span>
               <Arrow className="primary-button__icon" />
-            </button>
+            </NavLink>
             <button type="button" className="secondary-button">
               <Heart className="secondary-button__icon" />
               <span>Add to wish list</span>
--- src/pages/ProductsPage/index.tsx
+++ src/pages/ProductsPage/index.tsx
@@ -1,18 +1,22 @@
 import React, { FC } from 'react';
 
+import { useAppSelector } from '@hooks';
+import { selectProducts } from '@store/products/selectors';
+
 import { Container } from '@components/Container';
 import { ProductsList } from '@components/ProductsList';
 
 import './ProductsPage.scss';
 
 const ProductsPage: FC = () => {
+  const products = useAppSelector(selectProducts).length;
   return (
     <section className="products">
       <Container>
         <div className="products__title">
           <h1 className="products__heading">All Products</h1>
           <div className="products__quantity">
-            <span className="products__quantity--number">117</span>
+            <span className="products__quantity--number">{products}</span>
             <span>Products</span>
           </div>
         </div>
