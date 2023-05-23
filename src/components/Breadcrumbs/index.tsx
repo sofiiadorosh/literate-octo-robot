0006ddf0e936fcebded71a37ef38263c5e01f1c3
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-05-23 10:13:22 +0300

    feat: add breadcrumbs

--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -1,15 +1,74 @@
 import React, { FC } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+
+import { useAppSelector } from '@hooks';
+import { selectProducts } from '@store/products/selectors';
 
 import './Breadcrumbs.scss';
 
+interface BreadcrumbPath {
+  name: string;
+  link: string;
+}
+
 export const Breadcrumbs: FC = () => {
+  const products = useAppSelector(selectProducts);
+  const location = useLocation();
+  const { pathname } = location;
+
+  const getBreadcrumbPaths = (): BreadcrumbPath[] => {
+    const paths: BreadcrumbPath[] = [{ name: 'Homepage', link: '/' }];
+
+    const parts = pathname.split('/').filter(part => part !== '');
+    let accumulatedPath = '/';
+    for (let i = 0; i < parts.length; i++) {
+      accumulatedPath += parts[i];
+      const breadcrumbName = getBreadcrumbName(parts[i]);
+      paths.push({
+        name: breadcrumbName,
+        link: accumulatedPath,
+      });
+      accumulatedPath += '/';
+    }
+
+    return paths;
+  };
+
+  const getBreadcrumbName = (part: string): string => {
+    if (part === 'products') {
+      return 'All products';
+    }
+    const isProductId = /^\d+$/.test(part);
+    if (isProductId) {
+      const product = products.find(product => product.id === part);
+      if (product) {
+        return product.title;
+      }
+    }
+
+    return part.charAt(0).toUpperCase() + part.slice(1);
+  };
+
+  const breadcrumbPaths = getBreadcrumbPaths();
+  const lastPathIndex = breadcrumbPaths.length - 1;
+
   return (
     <ul className="breadcrumbs">
-      <li className="breadcrumbs__page">Homepage</li>
-      <li className="breadcrumbs__page">/</li>
-      <li className="breadcrumbs__page breadcrumbs__page_active">
-        All products
-      </li>
+      {breadcrumbPaths.map((path, index) => (
+        <li key={index} className="breadcrumbs__page">
+          {index !== 0 && <span> / </span>}
+          <NavLink
+            className={
+              lastPathIndex === index
+                ? 'breadcrumbs__link breadcrumbs__link_active'
+                : 'breadcrumbs__link'
+            }
+            to={path.link}
+          >
+            {path.name}
+          </NavLink>
+        </li>
+      ))}
     </ul>
   );
 };
